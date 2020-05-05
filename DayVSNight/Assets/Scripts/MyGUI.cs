e90b78be8a4822dc4963e40135d4867728192571
Josefin <josstrom@kth.se>
2020-05-05 19:51:56 +0200

    Added startbutton using GUI

---
+++ DayVSNight/Assets/Scripts/MyGUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGUI : MonoBehaviour
+{
+    public Texture aTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (!aTexture)  //Check that there is a texture
+        {
+            Debug.LogError("Assign a Texture in the inspector.");
+            return;
+        }
+
+        GUI.DrawTexture(new Rect(10, 10, 1600, 900), aTexture, ScaleMode.ScaleToFit, true, 10.0F);
+        // Make a background box for the start menu
+        GUI.Box(new Rect(200, 200, 100, 200), "Menu");
+
+        // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Start"))
+        {
+            Debug.Log("Start!");
+            //Application.LoadLevel(1);
+        }
+
+        // Make the second button, quit application if pressed.
+        if (GUI.Button(new Rect(20, 70, 80, 20), "Quit"))
+        {
+            Application.Quit();
+        }
+    }
+}
