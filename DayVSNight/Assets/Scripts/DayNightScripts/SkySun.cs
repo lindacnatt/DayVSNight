c01860b11c62b1650903ddf8153656b40de4d498
Josefin <josstrom@kth.se>
2020-05-10 13:49:15 +0200

    Sun and Moon rotation
    Just started, but committing to save :')

---
+++ DayVSNight/Assets/Scripts/DayNightScripts/SkySun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkySun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.right, 10f * Time.deltaTime);   // rotates around zero coordinates, around the right axis, at a defined speed. Change speed to somehow depend on health!
+        transform.LookAt(Vector3.zero);
+    }
+}
---
+++ DayVSNight/Assets/Scripts/MenuScripts/StartGUI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGUI : MonoBehaviour
+{
+    public Texture aTexture;
+    public GUIStyle styleBox;
+    public GUIStyle styleButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (!aTexture)  //Check that there is a texture
+        {
+            Debug.LogError("Assign a Texture in the inspector.");
+            return;
+        }
+        
+        int screenwidth = 700;
+        int buttonwidth = 80;
+        int buttonheight = 40;
+        float marginx = (1280-screenwidth)/2;
+        int marginy = 200;
+       
+
+        // GUI.DrawTexture(new Rect(150, 100, 1600, 900), aTexture, ScaleMode.ScaleToFit, true, 10.0F);  // Not quite good
+
+        // Make a background box for the start menu
+        GUI.DrawTexture(new Rect(0, 0, 1280, 800), aTexture, ScaleMode.ScaleToFit, true);
+
+
+        // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+        if (GUI.Button(new Rect(marginx+(screenwidth-buttonwidth)/2, marginy+140, buttonwidth, buttonheight), "Start"))
+        {
+            Debug.Log("Start!");
+            //Application.LoadLevel(1);
+        }
+
+        // Make the second button, quit application if pressed.
+        if (GUI.Button(new Rect(marginx + (screenwidth - buttonwidth) / 2, marginy + buttonheight + 160, buttonwidth, buttonheight), "Quit"))
+        {
+            Application.Quit();
+        }
+        GUI.skin.button = styleButton;
+
+    }
+}
