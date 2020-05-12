2ee89a90831f8e136892b8dd45a304351482eec7
Josefin <josstrom@kth.se>
2020-05-12 21:26:21 +0200

    Not sure what this is merging 1
    Should be merged with a functioning merge between Playfield and Day/Night cycle... But not sure anymore hahah don't even know what is commited but should *hopefully* work

---
+++ DayVSNight/Assets/Our Assets/Scripts/MenuScripts/StartGUI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGUI : MonoBehaviour
+{
+    public Texture aTexture;
+    public GUIStyle styleBox;
+    public GUIStyle styleButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+
+        if (!aTexture)  //Check that there is a texture
+        {
+            Debug.LogError("Assign a Texture in the inspector.");
+            return;
+        }
+        
+        int screenwidth = Screen.width;
+        int buttonwidth = 80;
+        int buttonheight = 40;
+        float marginx = (screenwidth-700)/2;
+        int marginy = 200;
+       
+
+        // GUI.DrawTexture(new Rect(150, 100, 1600, 900), aTexture, ScaleMode.ScaleToFit, true, 10.0F);  // Not quite good
+
+        // Make a background box for the start menu
+        GUI.DrawTexture(new Rect(0, 0, screenwidth, Screen.height), aTexture, ScaleMode.ScaleToFit, true);
+
+
+        // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+        if (GUI.Button(new Rect(marginx+(700-buttonwidth)/2, marginy+140, buttonwidth, buttonheight), "Start"))
+        {
+            Debug.Log("Start!");
+            //Application.LoadLevel(1);
+        }
+
+        // Make the second button, quit application if pressed.
+        if (GUI.Button(new Rect(marginx + (700 - buttonwidth) / 2, marginy + buttonheight + 160, buttonwidth, buttonheight), "Quit"))
+        {
+            Application.Quit();
+        }
+        GUI.skin.button = styleButton;
+
+    }
+}
