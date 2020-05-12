811aaeae3113c3abb8dc9974e81c20f4c4431a7c
Josefin <josstrom@kth.se>
2020-05-12 17:33:51 +0200

    Merged playfield and daynight cycle
    + Changed texture on mountains 
    - Errors with GUID

---
+++ DayVSNight/Assets/Our Assets/Scripts/DayNightScripts/DayNightCycle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class DayNightCycle : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.right, -50f);
+        transform.LookAt(Vector3.zero);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SunHit() {
+
+        transform.RotateAround(Vector3.zero, Vector3.right, 10f);
+        transform.LookAt(Vector3.zero);
+    }
+    public void MoonHit()
+    {
+
+        transform.RotateAround(Vector3.zero, Vector3.right, -10f);
+        transform.LookAt(Vector3.zero);
+    }
+
+    
+}
