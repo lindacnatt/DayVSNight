77ec6eaa5da23248683211bd05a20029e0bbaa58
lindacnatt <linda.cnattingius@gmail.com>
2020-05-19 01:13:14 +0200

    make moons only damage suns and vice versa
    Shooting 2 bullets now

--- DayVSNight/Assets/MoonShoot.cs
+++ DayVSNight/Assets/MoonShoot.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoonShoot : MonoBehaviour
-{
-    public int m_PlayerNumber = 1;              
+{           
     public Rigidbody m_Bullet;                  
-    public Transform m_FirePosition;          
+    public Transform m_FirePosition;
+    public Transform m_FirePosition2;           
     public float m_LaunchForce = 15f;  
     
     private float m_CurrentLaunchForce;    
@@ -31,12 +31,11 @@ public class MoonShoot : MonoBehaviour
 
     private void Fire ()
     {
-
         // Create an instance of the bullet and store a reference to it's rigidbody.
-        Rigidbody bulletInstance =
-            Instantiate (m_Bullet, m_FirePosition.position, m_FirePosition.rotation) as Rigidbody;
-
+        Rigidbody bulletInstance = Instantiate (m_Bullet, m_FirePosition.position, m_FirePosition.rotation) as Rigidbody;
+        Rigidbody bulletInstance2 = Instantiate (m_Bullet, m_FirePosition2.position, m_FirePosition2.rotation) as Rigidbody;
         // Set the bullet's velocity to the launch force in the fire position's forward direction.
-        bulletInstance.velocity = m_CurrentLaunchForce * m_FirePosition.forward; 
+        bulletInstance.velocity = m_CurrentLaunchForce * (m_FirePosition.right * -1); 
+        bulletInstance2.velocity = m_CurrentLaunchForce * (m_FirePosition2.forward); 
     } 
 }
